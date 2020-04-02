0d027cf77b0baeb8eccf3a7582cf567c7ed4f0e5
EEH <eli.holmes@noaa.gov>
2020-04-01 18:19:43 -0700

    fittings fun should return both pres and abundance models

--- R/brt_sdm.R
+++ R/brt_sdm.R
@@ -1,6 +1,8 @@
 #' SDM fit with BRT
 #'
-#' Build a boosted regression tree species distribution model from the output from a operating model using the dismo package.
+#' Build a boosted regression tree species distribution model from the output from a operating model using the dismo package. 
+#' Models fit to both the
+#' presence (pres column) and log(abundance) are returned.
 #'
 #' @param x (required) An operating model as output from one of the operating model functions (such as \code{sim <- \link{SimulateWorld}()} OR list with meta$abund_enviro and grid from the operating model (\code{sim$grid}).
 #' @param covariates Covariates to use in the SDM. Must be in the operating model output (in the columns of x$grid). If left off, all covariates in x (in x$meta$covariates) are used.
@@ -20,30 +22,24 @@
 #' 
 #' @export
 brt_sdm <- function(x, covariates=NULL,
-                    response=c("pres", "abundance"),
                     start.forecast.year=2021){
   if(!inherits(x, "OM") & !all(c("meta", "grid") %in% names(x))) stop("Something is wrong. x should be a OM (operating model) object or a list with meta and grid.")
   if(!(c("abund_enviro") %in% names(x$meta))) stop("Something is wrong. x$meta needs 'abund_enviro' value.")
   abund_enviro <- x$meta$abund_enviro
   if(missing(covariates)) covariates <- x$meta$covariates
   
-  resp <- match.arg(response)
   if(!all(covariates %in% colnames(x$grid))) stop("The operating model does not have all the covariates specified.")
   
   # --- Data set-up section ----
-  # The repsonse variable (pres or abundance) is put in new 'resp' column
-  # If needed, resp is transformed (0s removed and logged or rounded)
   dat <- x$grid
-  # add a column with name resp to use in the fitting functions
-  dat$resp <- dat[,resp]
   
-  # If lognormal and resp is abundance, make the resp column logged
-  if (resp=="abundance" && str_detect(abund_enviro, "lnorm")){
-    dat <- dat[dat$abundance>0,] # remove 0s
-    dat$resp <- log(dat$resp) # log
+  # If lognormal and response is abundance, make the resp column logged
+  if ((abund_enviro == "lnorm_low" || abund_enviro == "lnorm_high")){
+    dat$log.abundance <- log(dat$abundance) # log
+    dat$log.abundance[dat$abundance==0] <- NA # remove 0s
   }
-  if (resp=="abundance" && abund_enviro == "poisson"){
-    dat$resp <- round(dat$resp)
+  if (abund_enviro == "poisson"){
+    dat$round.abundance <- round(dat$abundance)
   }
   
   #Create dataframe with historical/forecast data
@@ -51,24 +47,35 @@ brt_sdm <- function(x, covariates=NULL,
   dat_fcast <- dat[dat$year>=start.forecast.year,]
   
   # --- Model fitting section ----
-  # The response is in the resp column and has been transformed (if needed) already
+
+  ## --- Response fit
   
-  if(resp=="pres") fam <- "bernoulli"
-  if(resp=="abundance" & str_detect(abund_enviro, "lnorm")) fam <- "gaussian"
-  if(resp=="abundance" & abund_enviro=="poisson") fam <- "poisson"
-  
-  fit <- dismo::gbm.step(
+  fam <- "bernoulli"
+  fit.p <- dismo::gbm.step(
     data=dat_hist, 
     gbm.x = covariates, 
-    gbm.y = 'resp', 
+    gbm.y = 'pres', 
+    family = fam, 
+    tree.complexity = 3, 
+    learning.rate = 0.01, 
+    bag.fraction = 0.6)
+
+  ## --- Abundance fit
+  
+  if(str_detect(abund_enviro, "lnorm")){ fam <- "gaussian"; resp <- "log.abundance" }
+  if(abund_enviro=="poisson"){ fam <- "poisson"; resp <- "round.abundance" }
+  fit.a <- dismo::gbm.step(
+    data=dat_hist, 
+    gbm.x = covariates, 
+    gbm.y = resp, 
     family = fam, 
     tree.complexity = 3, 
     learning.rate = 0.01, 
     bag.fraction = 0.6)
   
   # Add on the meta info from the OM object
-  fit <- list(fit, meta=x$meta, sdm.response=resp)
-  class(fit) <- c(class(fit), "brt")
+  fit <- list(presence=fit.p, abundance=fit.a, meta=x$meta)
+  class(fit) <- c(class(fit), "brt", "SDM")
 
   return(fit)
 }
