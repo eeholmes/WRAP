4baa945a84ad0d9fe005b9e808bf3ca5d6ae91a9
EEH <eli.holmes@noaa.gov>
2020-04-01 17:32:03 -0700

    add package help page

---
+++ R/pkgname.R
@@ -0,0 +1,9 @@
+#' @title 
+#' WRAP Package
+#' @description 
+#' This package creates simulated presence/absence and abundance fields and
+#' fits SDMs.
+#' @keywords internal
+#' @importFrom dplyr "%>%"
+"_PACKAGE"
+
