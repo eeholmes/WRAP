b71940c9673877780aee5dff4c8a5c1b3e6d64d8
EEH <eli.holmes@noaa.gov>
2020-05-12 18:53:32 -0700

    add silent

--- R/brt_sdm.R
+++ R/brt_sdm.R
@@ -8,6 +8,7 @@
 #' @param covariates Covariates to use in the SDM. Must be in the operating model output (in the columns of x$grid). If left off, all covariates in x (in x$meta$covariates) are used.
 #' @param start.forecast.year The years less will be used for fitting and the years greater than are the forecasted years.
 #' @param control Parameters for the `dismo::gbm.step` call.
+#' @param silent Whether to output info from `dismo::gbm.step` during fitting.
 #' 
 #' @return A list with the presence and abundance fits and the meta data.
 #'
@@ -26,13 +27,16 @@
 #' @export
 brt_sdm <- function(x, covariates=NULL,
                     start.forecast.year=2021,
-                    control = list(tree.complexity = 3, learning.rate = 0.01, bag.fraction = 0.6)){
+                    control = list(tree.complexity = 3, 
+                                   learning.rate = 0.01, 
+                                   bag.fraction = 0.6),
+                    silent=TRUE){
   if(!inherits(x, "OM") & !all(c("meta", "grid") %in% names(x))) stop("Something is wrong. x should be a OM (operating model) object or a list with meta and grid.")
   if(!(c("abund_enviro") %in% names(x$meta))) stop("Something is wrong. x$meta needs 'abund_enviro' value.")
   abund_enviro <- x$meta$abund_enviro
   if(missing(covariates)) covariates <- x$meta$covariates
-  
   if(!all(covariates %in% colnames(x$grid))) stop("The operating model does not have all the covariates specified.")
+  if(!all(c("tree.complexity", "learning.rate", "bag.fraction") %in% names(control))) stop("control needs to have tree.complexity, learning.rate and bag.fraction.")
   
   # --- Data set-up section ----
   dat <- x$grid
@@ -60,7 +64,8 @@ brt_sdm <- function(x, covariates=NULL,
     family = fam, 
     tree.complexity = control$tree.complexity, 
     learning.rate = control$learning.rate, 
-    bag.fraction = control$bag.fraction)
+    bag.fraction = control$bag.fraction,
+    silent=silent)
   
   ## --- Abundance fit
   
@@ -76,7 +81,8 @@ brt_sdm <- function(x, covariates=NULL,
     family = fam, 
     tree.complexity = control$tree.complexity, 
     learning.rate = control$learning.rate, 
-    bag.fraction = control$bag.fraction)
+    bag.fraction = control$bag.fraction,
+    silent=silent)
   
   # Add on the meta info from the OM object
   fit <- list(presence=fit.p, abundance=fit.a, 
