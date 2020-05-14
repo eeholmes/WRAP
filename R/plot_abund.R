0985d962faf904d3c1e72725e6af0cb3497b8d18
EEH <eli.holmes@noaa.gov>
2020-05-13 21:04:07 -0700

    allow start.forecast.year to be less that max year

--- R/plot_abund.R
+++ R/plot_abund.R
@@ -2,13 +2,20 @@
 #' 
 #' plots the total abundance versus time
 #' 
+#' The SDMs which will be compared are \link[=SDM_class]{SDM} objects and
+#'  have the `start.forecast.year` as part of their meta data (in $meta).
+#'  The average abudance will be plotted for the training data (to which the SDM was fit) 
+#'  and the test data (after `start.forecast.year`). If `start.forecast.year`
+#'  is greater than the last year of data, then the forecast years are not
+#'  plotted (since there are not data for those years).
+#'  
 #' @param x An \code{\link[=OMclass]{OM}} object output by one of the simulation functions.
-#' @param ... SDMs to compare. Must be an \link[=SDM_class]{SDM} object output from one of the fitting functions:
-#' \code{\link{gam_sdm}}, \code{\link{brt_sdm}}, or \code{\link{mlp_sdm}}.
+#' @param ... SDMs to compare. Must be an \link[=SDM_class]{SDM} object output from one of the fitting functions (\code{\link{gam_sdm}},
+#'  \code{\link{brt_sdm}}, or \code{\link{mlp_sdm}}) or \link[=POM_class]{POM} object output by \code{\link{predict.OM}}.
 #' 
 #' @examples
 #' \dontrun{
-#' sim <- SimulateWorld()
+#' sim <- SimulateWorld(start.year=2015, n.year=20)
 #' mlp.fit <- mlp_sdm(sim, "temp")
 #' plot_abund(sim, mlp.fit)
 #' }
@@ -24,29 +31,35 @@ plot_abund <- function(x, ...) {
   mod.names <- stringr::str_trim(stringr::str_split(cl, ",")[[1]])
   mod.names <- mod.names[-1]
   
-  # First compute the true cog
+  # First compute the true abundance
   if (abund_enviro == "poisson") x$grid$abundance <- round(x$grid$abundance)
   abund <- stats::aggregate(abundance~year, x$grid, FUN="sum")
   abund$model <- "true"
   abund <- abund[,c("year", "model", "abundance")]
-
+  
   nm <- length(sdms)
   if(nm != 0){
     abund_enviro <- x$meta$abund_enviro
-    
+    fyr <- NULL
     for(i in 1:nm){
-      if(!inherits(sdms[[i]], "SDM")) stop("plot_cog requires SDM objects as returned by one of the fitting functions. See ?plot_cog for info.")
+      if(!inherits(sdms[[i]], "SDM") & !inherits(sdms[[i]], "POM")) stop("plot_cog requires SDM or POM objects as returned by one of the fitting functions or predict.OM. See ?plot_abund for info.")
+    if(inherits(sdms[[i]], "SDM"))
+      fyr <- c(fyr, sdms[[i]]$meta$start.forecast.year)
+    if(inherits(sdms[[i]], "POM"))
+      fyr <- c(fyr, attr(sdms[[i]], "start.forecast.year"))
     }
-    fyr <- unique(unlist(lapply(sdms, function(x){x$meta$start.forecast.year})))
-    if(length(fyr) != 1) stop("The start.forecast.year in the SDM objects (x$meta) is different.")
+    fyr <- unique(fyr)
+    if(length(fyr) != 1) stop("The start.forecast.year in the SDM (or POM) objects are different.")
     
     pred.all <- NULL
     for(i in 1:nm){
       mod <- sdms[[i]]
-      mod.a <- mod$abundance
-      mod.p <- mod$presence
+      if(inherits(sdms[[i]], "SDM")){
       cat("getting prediction with", mod.names[i], "\n")
-      pred <- stats::predict(x, p.sdm=mod.p, a.sdm=mod.a, silent=TRUE)
+      pred <- stats::predict(x, sdm=mod, silent=TRUE)
+      }else{
+        pred <- mod
+      }
       pred$model <- mod.names[i]
       pred.all <- dplyr::bind_rows(pred.all, pred)
     }
@@ -56,11 +69,20 @@ plot_abund <- function(x, ...) {
     colnames(tmp) <- c("year", "model", "abundance")
     abund <- rbind(abund, tmp)
   }
+  abund$date <- as.Date(paste(abund$year, 1, 1, sep = "-"))
   
-  p <- ggplot(abund, aes(x=year, y=abundance, color=model)) + 
+  p <- ggplot(abund, aes(x=date, y=abundance, color=model)) + 
     geom_line() +
-    ggtitle("Comparison of lnorm_low models") +
-    geom_vline(xintercept=fyr-0.5) +
-    annotate("text", x=fyr-0.5, y=max(abund$abundance), label="  forecast", hjust=0) +
-    annotate("text", x=fyr-0.5, y=max(abund$abundance), label="hindcast  ", hjust=1)
-  p}
+    ggtitle("Average Abundance") +
+    ylab("Abundance") +
+    scale_x_date(date_labels = "%Y") +
+    xlab("Year")
+  if(fyr < max(abund$year)){
+    fyr.date <- as.Date(paste(fyr, 1, 1, sep="-")) - 182
+    p <- p +
+      geom_vline(xintercept=fyr.date) +
+      annotate("text", x=fyr.date, y=max(abund$abundance), label="  forecast", hjust=0) +
+      annotate("text", x=fyr.date, y=max(abund$abundance), label="hindcast  ", hjust=1)
+  }
+  p
+}
--- R/plot_cog.R
+++ R/plot_cog.R
@@ -2,16 +2,21 @@
 #' 
 #' plots the center of gravity versus time
 #' 
+#' The SDMs which will be compared are \link[=SDM_class]{SDM} objects and
+#'  have the `start.forecast.year` as part of their meta data (in $meta).
+#'  The COG will be plotted for the training data (to which the SDM was fit) 
+#'  and the test data (after `start.forecast.year`). If `start.forecast.year`
+#'  is greater than the last year of data, then the forecast years are not
+#'  plotted (since there is not data for those years).
+#' 
 #' @param x An \code{\link[=OMclass]{OM}} object output by one of the simulation functions.
-#' @param ... SDMs to compare. Must be an \link[=SDM_class]{SDM} object output from one of the fitting functions:
-#' \code{\link{gam_sdm}}, \code{\link{brt_sdm}}, or \code{\link{mlp_sdm}}.
+#' @param ... SDMs to compare. Must be an \link[=SDM_class]{SDM} object output from one of the fitting functions (\code{\link{gam_sdm}},
+#'  \code{\link{brt_sdm}}, or \code{\link{mlp_sdm}}) or \link[=POM_class]{POM} object output by \code{\link{predict.OM}}.
 #' 
 #' @examples
-#' \dontrun{
-#' sim <- SimulateWorld()
+#' sim <- SimulateWorld(start.year=2015, n.year=20)
 #' mlp.fit <- mlp_sdm(sim, "temp")
 #' plot_cog(sim, mlp.fit)
-#' }
 #' 
 #' @export
 plot_cog <- function(x, ...) {
@@ -25,43 +30,55 @@ plot_cog <- function(x, ...) {
   mod.names <- mod.names[-1]
   
   # First compute the true cog
-  cog_lat <- x$grid %>% dplyr::group_by(year) %>% 
-    dplyr::summarize(cog=stats::weighted.mean(x=lat, w=abundance))
+  # this returns a grouped data frame needed for the next step
+  cog_lat <- dplyr::group_by(x$grid, year) 
+  cog_lat <- dplyr::summarize(cog_lat, cog=stats::weighted.mean(x=lat, w=abundance))
   cog_lat <- cbind(model="true", cog_lat, stringsAsFactors = FALSE)
   
   nm <- length(sdms)
   if(nm != 0){
-    for(i in 1:nm)
-      if(!inherits(sdms[[i]], "SDM")) stop("plot_cog requires SDM objects as returned by one of the fitting functions. See ?plot_cog for info.")
-  fyr <- unique(unlist(lapply(sdms, function(x){x$meta$start.forecast.year})))
-  if(length(fyr) != 1) stop("The start.forecast.year in the SDM objects (x$meta) is different.")
+    fyr <- NULL
+    for(i in 1:nm){
+      if(!inherits(sdms[[i]], "SDM") & !inherits(sdms[[i]], "POM")) stop("plot_cog requires SDM or POM objects as returned by one of the fitting functions or predict.OM. See ?plot_abund for info.")
+      if(inherits(sdms[[i]], "SDM"))
+        fyr <- c(fyr, sdms[[i]]$meta$start.forecast.year)
+      if(inherits(sdms[[i]], "POM"))
+        fyr <- c(fyr, attr(sdms[[i]], "start.forecast.year"))
+    }
+    fyr <- unique(fyr)
+    if(length(fyr) != 1) stop("The start.forecast.year in the SDM (or POM) objects are different.")
     
     pred.all <- NULL
     for(i in 1:nm){
       mod <- sdms[[i]]
-      mod.a <- mod$abundance
-      mod.p <- mod$presence
       cat("getting prediction with", mod.names[i], "\n")
-      pred <- predict(x, p.sdm=mod.p, a.sdm=mod.a, silent=TRUE)
+      pred <- predict(x, sdm=mod, silent=TRUE)
       pred$model <- mod.names[i]
       pred.all <- dplyr::bind_rows(pred.all, pred)
     }
     
     # Now add the predicted cogs
-    tmp <- pred.all %>% dplyr::group_by(model, year) %>% 
-      dplyr::summarize(cog=stats::weighted.mean(x=lat, w=pred))
+    tmp <-  dplyr::group_by(pred.all, model, year) 
+    tmp <-  dplyr::summarize(tmp, cog=stats::weighted.mean(x=lat, w=pred))
     
     # dplyr uses tibbles and they return a matrix when you use rbind(). ug.
     # so use 
     cog_lat <- dplyr::bind_rows(cog_lat, tmp)
   }
-  p <- ggplot(cog_lat, aes(x=year, y=cog, color=model)) + 
+  cog_lat$date <- as.Date(paste(cog_lat$year, 1, 1, sep = "-"))
+  p <- ggplot(cog_lat, aes(x=date, y=cog, color=model)) + 
     geom_line() +
     ggtitle("Center of Gravity") +
     ylab("Centre of Gravity (deg lat)") +
-    geom_vline(xintercept=fyr-0.5) +
-    annotate("text", x=fyr-0.5, y=max(cog_lat$cog), label="  forecast", hjust=0) +
-    annotate("text", x=fyr-0.5, y=max(cog_lat$cog), label="hindcast  ", hjust=1)
-  
+    scale_x_date(date_labels = "%Y") +
+    xlab("Year")
+  if(fyr < max(cog_lat$year)){
+    fyr.date <- as.Date(paste(fyr, 1, 1, sep="-")) - 182
+    p <- p +
+    geom_vline(xintercept=fyr.date) +
+    annotate("text", x=fyr.date, y=max(cog_lat$cog), label="  forecast", hjust=0) +
+    annotate("text", x=fyr.date, y=max(cog_lat$cog), label="hindcast  ", hjust=1)
+  }
+    
   p
 }
